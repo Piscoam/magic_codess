a4cd4c76ce40bd845c604c82b7548350a01a8261
Pisco <83592195+Piscoam@users.noreply.github.com>
2022-04-28 11:27:03 +0430

    Add files via upload

---
+++ magic_codes.py
@@ -0,0 +1,291 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().run_line_magic('notebook', '')
+get_ipython().run_line_magic('pinfo', '')
+
+
+# In[2]:
+
+
+get_ipython().run_line_magic('notebook', '')
+
+
+# In[3]:
+
+
+get_ipython().run_line_magic('pinfo', '')
+
+
+# In[4]:
+
+
+get_ipython().run_line_magic('precision', '')
+
+
+# In[5]:
+
+
+get_ipython().run_line_magic('pwd', '')
+
+
+# In[6]:
+
+
+get_ipython().run_line_magic('pylab', '')
+
+
+# In[7]:
+
+
+get_ipython().run_line_magic('recall', '')
+
+
+# In[ ]:
+
+
+C:\Users\Pisco
+
+
+# In[8]:
+
+
+get_ipython().run_line_magic('pylab', '')
+
+
+# In[9]:
+
+
+get_ipython().run_line_magic('recall', '')
+
+
+# In[ ]:
+
+
+C:\Users\Pisco
+
+
+# In[10]:
+
+
+get_ipython().run_line_magic('run', '')
+
+
+# In[ ]:
+
+
+get_ipython().run_line_magic('timeit', '')
+
+
+# In[11]:
+
+
+get_ipython().run_line_magic('who', '')
+
+
+# In[12]:
+
+
+get_ipython().run_line_magic('autocall', '')
+
+
+# In[13]:
+
+
+get_ipython().run_line_magic('autocall', '{mode}')
+
+
+# In[14]:
+
+
+
+
+
+# In[ ]:
+
+
+get_ipython().run_line_magic('pdw', '')
+
+
+# In[ ]:
+
+
+
+
+
+# In[15]:
+
+
+get_ipython().run_line_magic('cd', '')
+
+
+# In[16]:
+
+
+get_ipython().run_line_magic('dhist', '')
+
+
+# In[17]:
+
+
+get_ipython().run_line_magic('edit', '')
+
+
+# In[18]:
+
+
+get_ipython().run_line_magic('env', '')
+
+
+# In[19]:
+
+
+get_ipython().run_line_magic('env', 'var')
+
+
+# In[ ]:
+
+
+get_ipython().run_line_magic('env', 'var wall')
+
+
+# In[20]:
+
+
+get_ipython().run_line_magic('env', 'var wall')
+
+
+# In[21]:
+
+
+get_ipython().run_line_magic('gui', '')
+
+
+# In[22]:
+
+
+get_ipython().run_line_magic('gui', 'wx')
+
+
+# In[23]:
+
+
+get_ipython().run_line_magic('gui', 'qt4')
+
+
+# In[24]:
+
+
+get_ipython().run_line_magic('gui', 'qt4 | qt')
+
+
+# In[25]:
+
+
+get_ipython().run_line_magic('lsmagic', '')
+
+
+# In[26]:
+
+
+get_ipython().run_line_magic('matlotlib', '')
+
+
+# In[27]:
+
+
+get_ipython().run_line_magic('notebook', '')
+
+
+# In[28]:
+
+
+get_ipython().run_line_magic('xinfo', '')
+
+
+# In[29]:
+
+
+get_ipython().run_line_magic('pinfo', '')
+
+
+# In[30]:
+
+
+get_ipython().run_line_magic('precision', '')
+
+
+# In[31]:
+
+
+get_ipython().run_line_magic('wpd', '')
+
+
+# In[32]:
+
+
+wpd
+
+
+# In[33]:
+
+
+get_ipython().run_line_magic('pdw', '')
+
+
+# In[34]:
+
+
+get_ipython().run_line_magic('automagic', '')
+
+
+# In[35]:
+
+
+get_ipython().run_line_magic('pdw', '')
+
+
+# In[36]:
+
+
+get_ipython().run_line_magic('pdw', '')
+
+
+# In[37]:
+
+
+pdw
+
+
+# In[38]:
+
+
+get_ipython().run_line_magic('pylab', '')
+
+
+# In[39]:
+
+
+get_ipython().run_line_magic('run', '')
+
+
+# In[40]:
+
+
+get_ipython().run_line_magic('timeit', '')
+
+
+# In[41]:
+
+
+get_ipython().run_line_magic('who', '')
+
+
+# In[ ]:
+
+
+
+
